7c5ab3cbbf7a6fbe8f20515083dd5188b40befde
Sirajuddin Choudhary <sirajuddin.c@gmail.com>
2016-07-01 07:29:51 +0530

    chore: upgrade to rc4, router beta.2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { provideRouter, RouterConfig } from '@angular/router';
 import { HeroComponent, HERO_ROUTES } from './hero';
 
 export const APP_ROUTES: RouterConfig = [
-  { path: '', redirectTo: 'hero', terminal: true },
+  { path: '', redirectTo: 'hero', pathMatch: 'full' },
   { path: 'hero',
     component: HeroComponent,
     children: [
       ...HERO_ROUTES
     ]
-   }
+  },
+  { path: '**', redirectTo: 'hero', pathMatch: 'full' }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
